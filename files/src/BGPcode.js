e054e99f58bf88706568c1389f8bbe99107258c8
BledyGames <97191004+BGP100@users.noreply.github.com>
2023-02-07 10:31:28 -0600

    Update BGPcode.js

--- files/src/BGPcode.js
+++ files/src/BGPcode.js
@@ -375,5 +375,23 @@ var BGPcode = {
     }
     const blob = new Blob(byteArrays, {type: t});
     return URL.createObjectURL(blob).toString();
+  },
+  getCookie: function(cookie){
+    function hello(cookieName) {
+      const name = cookieName + "=";
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const ca = decodedCookie.split(';');
+      for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+    eval(hello(cookie));
   }
 };
